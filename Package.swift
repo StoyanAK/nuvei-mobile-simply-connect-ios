aa5b5ab704f659c1e6466dd26bd5837cf51bba42
StoyanAK <70704378+StoyanAK@users.noreply.github.com>
2024-12-17 17:30:10 +0200

    update package.siwft

--- Package.swift
+++ Package.swift
@@ -1,6 +1,3 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -13,14 +10,10 @@ let package = Package(
             name: "NuveiSimplyConnectSDK",
             targets: ["NuveiSimplyConnectSDKWrapper"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/airsidemobile/JOSESwift", from: "2.4.0"),
-    ],
     targets: [
         .target(
             name: "NuveiSimplyConnectSDKWrapper",
             dependencies: [
-                .product(name: "JOSESwift", package: "JOSESwift"),
                 .target(name: "NuveiSimplyConnectSDKFramework")
             ],
             path: "SwiftPM/NuveiSimplyConnectSDKWrapper"
